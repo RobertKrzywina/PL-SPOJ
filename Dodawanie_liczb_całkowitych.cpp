cc4494d59a9234714e42bcb10be0dde052fb47c0
Robert Krzywina <36728025+RobertKrzywina@users.noreply.github.com>
2018-02-22 11:47:54 +0100

    1 plik

---
+++ Dodawanie_liczb_całkowitych.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int liczba[3], suma;
+  
+	cin>>liczba[0];
+	cin>>liczba[1];
+	cin>>liczba[2];
+	suma=liczba[0]+liczba[1]+liczba[2];
+	cout<<suma;
+	
+	return 0;
+}
