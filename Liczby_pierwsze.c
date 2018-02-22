9d6a0ac19682e031ce72ee351062784b9e139e7b
Robert Krzywina <36728025+RobertKrzywina@users.noreply.github.com>
2018-02-22 11:04:03 +0100

    1 plik

---
+++ Liczby_pierwsze.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, k, j, p;
+    scanf("%d", &n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &k);
+        p=0;
+        if(k>=2){
+            for(j=2;j<k;j++)
+            {
+                if(k%j==0){
+                p++;
+                }
+            }
+        }
+        if(p!=0 && k>=2) puts("NIE");         
+        else if(p==0 && k>=2) puts("TAK");
+        else puts("NIE");
+    }
+
+    return 0;
+}
