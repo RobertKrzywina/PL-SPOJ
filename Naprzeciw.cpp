30e671a827a6d993334eb7dc3e857c41baecdf9e
Robert Krzywina <36728025+RobertKrzywina@users.noreply.github.com>
2018-03-25 15:18:07 +0200

    1 plik

---
+++ Naprzeciw.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned int t, n, k;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>k;
+        if(n%2) cout<<"BRAK"<<endl;
+        else{
+            if(k>n/2) cout<<k-n/2<<endl;
+            else cout<<k+n/2<<endl;
+        }
+    }
+
+    return 0;
+}
